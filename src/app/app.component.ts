f4ff4d8cb60252cedcf807de5d95bea336afc705
azure1 <abdo.samir.7719@gmail.com>
2024-05-09 23:49:58 +0300

    add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './Shared/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , CommonModule],
+  imports: [RouterOutlet , CommonModule ,NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [
